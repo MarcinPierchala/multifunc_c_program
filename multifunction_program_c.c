52d34820273d90c5c5c63ad3dd5f1c1a6f6a1025
MarcinPierchala <macintecmarcin@gmail.com>
2021-09-20 11:58:24 +0200

    after creation next function - tool to center txt in window - nees repair because centering 1 word only

--- multifunction_program_c.c
+++ multifunction_program_c.c
@@ -2,10 +2,13 @@
 
 float calc();
 int menu();
+int textCentering();
 int choice;
 
+
+
 int main(void) {
-  
+  printf("WITAJ W PROGRAMIE WIELOFUNKCYJNYM NAPISANYM W C\n");
   menu();
 
   
@@ -23,22 +26,47 @@ float calc(){
   scanf("%s", &action);
 
   switch(action){
-    case('+'): printf("wynik dodawania : %f", a+b); break;
-    case('-'): printf("wynik odejmowania : %f", a-b); break;
-    case('*'): printf("wynik mnozenia : %f", a*b); break;
-    case('/'): printf("wynik dzielenia : %f", a/b); break;
-    default:printf("podales zly znak dzialania\nuruchom program jeszcze raz");
+    case('+'): printf("wynik dodawania : %f\n", a+b); break;
+    case('-'): printf("wynik odejmowania : %f\n", a-b); break;
+    case('*'): printf("wynik mnozenia : %f\n", a*b); break;
+    case('/'): printf("wynik dzielenia : %f\n", a/b); break;
+    default:printf("podales zly znak dzialania\nuruchom program jeszcze raz\n");
     
   }
-  return 0;
+  return menu();
 }
 
 int menu(){
-  printf("witaj w programie wielofunkcyjnym napisanym w C\nMENU:\n1: KALKULATOR\n0: KONIEC I WYJSCIE Z PROGRAMU\n");
+  printf("MENU:\n1: KALKULATOR\n2: NARZEDZIE DO WYSRODKOWANIA TEKSTU\n0: KONIEC I WYJSCIE Z PROGRAMU\n");
   scanf("%d", &choice);
   switch(choice){
     case(0): printf("WYBRANO ZAKONCZENIE PROGRAMU\nDZIEKUJE\nMARTIN");break;
     case(1): printf("WYBRANO KALKULATOR\nPOSTEPUJ ZGODNIE Z INSTRUKCJAMI\n"); calc(); break;
+    case(2): printf("WYBRANO NARZEDZIE DO WYSRODKKOWANIA TEXTU W OKIENKU\n"); textCentering();break;
   }
   return 0;
+}
+
+int textCentering(){
+  //na razie dobrze centruje tylko 1 wyraz 
+  //każda spacja robi błąd
+  typedef char* String;
+  int width;
+  int size = 0;
+  printf("PODAJ SZEROKOSC OKIENKA DO WYSWIETLANIA TEXTU : \n");
+  scanf("%d", &width);
+  char message [width];
+  printf("PODAJ TEXT DO WYŚRODKOWANIA : \n");
+  scanf("%s", message);
+  printf("WPISANO TEXT : %s\n", message);
+  char* poi = message;
+  while(*poi){
+    size++;
+    poi++;
+  }
+  for(int i = 0;i<((width-size)/2);i++) printf(" ");
+  printf("%s", message);
+  printf("\n\n");
+  
+  return menu();
 }
